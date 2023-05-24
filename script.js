aefd70c9095fba0630ea4e2b63a588240d324891
MuhammadN0 <117872290+MuhammadN0@users.noreply.github.com>
2023-05-24 18:21:08 +0300

    Made screen real

--- script.js
+++ script.js
@@ -1,39 +1,83 @@
 const avSeats = document.querySelectorAll('.rows .row .seat:not(.occupied)');
-const count = document.getElementById('count')
-const price = document.getElementById('price')
+const allSeats = document.querySelectorAll('.rows .row .seat');
+const count = document.getElementById('count');
+const price = document.getElementById('price');
+const submit = document.querySelector('.submit');
 let selectedArray = [];
+let occupiedArray = [];
+let submittedArray = [];
 
-if (sessionStorage.getItem('selectedArray')){
-  selectedArray = JSON.parse(sessionStorage.getItem('selectedArray'))
-  selectedArray.forEach(e => {
+if (sessionStorage.getItem('selectedArray')) {
+  selectedArray = JSON.parse(sessionStorage.getItem('selectedArray'));
+  selectedArray.forEach((e) => {
     avSeats.forEach((el, ind) => {
       if (ind == e) {
-        el.classList.add('selected')
+        el.classList.add('selected');
       }
-    })
-  })
-  count.innerHTML = selectedArray.length
+    });
+  });
+  count.innerHTML = selectedArray.length;
   price.innerHTML = selectedArray.length * 10;
 }
 
-avSeats.forEach((e,index) => {
-  e.addEventListener('click', ()=>{
-    e.classList.toggle('selected')
-    isSelected = e.classList.contains('selected');
-    updatePrice(index, isSelected);
-  })
-})
 
-
-function updatePrice (index, isSelected) {
-  if (isSelected){
-    selectedArray.push(index)
-    sessionStorage.setItem('selectedArray',JSON.stringify(selectedArray))
-  }else{
-    let newSelectedArray = selectedArray.filter(e => e != index);
+function updatePrice(index, isSelected) {
+  if (isSelected) {
+    selectedArray.push(index);
+    sessionStorage.setItem('selectedArray', JSON.stringify(selectedArray));
+  } else {
+    let newSelectedArray = selectedArray.filter((e) => e != index);
     selectedArray = newSelectedArray;
-    sessionStorage.setItem('selectedArray',JSON.stringify(selectedArray))
+    sessionStorage.setItem('selectedArray', JSON.stringify(selectedArray));
   }
-  count.innerHTML = selectedArray.length
+  count.innerHTML = selectedArray.length;
   price.innerHTML = selectedArray.length * 10;
 }
+
+fetch('https://react-http-747fe-default-rtdb.firebaseio.com/ticketss.json')
+  .then((data) => {
+    return data.json();
+  })
+  .then((data) => {
+    for (const key in data) {
+      occupiedArray.push(data[key]);
+    }
+    let newOccupied = occupiedArray.slice(-1);
+    occupiedArray[0] = newOccupied[0];
+    occupiedArray[0].forEach((e) => {
+      avSeats.forEach((el, ind) => {
+        if (ind == e) {
+          el.classList.add('occupied');
+        }
+      });
+    });
+    avSeats.forEach((e, index) => {
+      e.addEventListener('click', () => {
+        e.classList.toggle('selected');
+        isSelected = e.classList.contains('selected');
+        updatePrice(index, isSelected);
+      });
+    });
+    
+  });
+
+submit.addEventListener('click', () => {
+  avSeats.forEach((e, i) => {
+    if (e.classList.contains('occupied') || e.classList.contains('selected')) {
+      submittedArray.push(i);
+    }
+  });
+  fetch('https://react-http-747fe-default-rtdb.firebaseio.com/ticketss.json', {
+    method: 'POST',
+    body: JSON.stringify(submittedArray),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((data) => {
+    count.innerHTML = 0;
+    price.innerHTML = 0;
+    sessionStorage.removeItem('selectedArray');
+    window.location.reload();
+  });
+});
+//https://react-http-747fe-default-rtdb.firebaseio.com/objects.json
